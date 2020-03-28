39d8741e05ede4d493ccab20d99ff95e439effd5
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-28 01:09:18 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,18 @@
+
 class MP3Importer
 
-  def initialize(path)
-    @path = path
-  end
+attr_accessor :path
 
+def initialize(path)
+  @path = path
+end
 
+def files
+  @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+end
 
+def import
+  files.each {|song| Song.new_by_filename(song)}
+end
 
 end
