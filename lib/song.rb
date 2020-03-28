c73fa6743ab631b9825be143bf01c37a60468913
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-28 02:06:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,16 @@ class Song
     @@all
   end
 
-    def self.new_by_filename(file)
+  def self.new_by_filename(file)
      song_info = file.chomp(".mp3").split(" - ")
      song = Song.new(song_info[1])
      song.artist= song_info[0]
      song
-end
+   end
+
+   def artist_name=(name)
+       self.artist = Artist.find_or_create_by_name(name)
+       artist.add_song(self)
+     end
 
 end
