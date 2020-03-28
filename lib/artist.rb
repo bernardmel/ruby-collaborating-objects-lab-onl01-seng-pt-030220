feccc349a587b077a2d4ce310fd2e99dbf68b9bf
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-28 00:52:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,13 +12,7 @@ class Artist
     @songs << song
   end
 
-  def save
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
+  
 
   def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
