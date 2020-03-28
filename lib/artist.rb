135c33ed9cb78980cf67e79865c46470ecb825fa
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-28 00:40:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,24 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :song
 
   @@all = []
 
   def initialize(name)
     @name = name
-
+    @song = song
   end
 
   def self.all
     @@all
   end
+
+  def add_song(song)
+    @song << song
+  end 
+
+
+
+
 
 end
