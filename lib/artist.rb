f4abf5fb01eabbf9ce4af434fb7fff237796e491
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-28 01:45:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,10 +13,21 @@ class Artist
   def self.all
      @@all 
   end
+  
+  def self.find_or_create_by_name(artist_name)
+     found_artist = self.all.find {|artist| artist.name == artist_name}
+     if found_artist
+       found_artist
+     else
+       new_artist = self.new(artist_name)
+       new_artist.save
+       new_artist
+     end
+   end
 
-
-  def add_song(song)
-    @songs << song
-  end
-
-end
+   def print_songs
+     @songs.each do |song|
+       puts song.name
+     end
+   end
+ end
